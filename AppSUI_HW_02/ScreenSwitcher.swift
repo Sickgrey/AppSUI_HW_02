dbfa6c5540f70ee5f35883ffebbe3c186037e72a
Sergey <fangooner.ars@gmail.com>
2023-08-02 16:57:43 +0200

    added navigation stack

--- AppSUI_HW_02/ScreenSwitcher.swift
+++ AppSUI_HW_02/ScreenSwitcher.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UISystem
 
 struct ScreenSwitcher: View {
     
@@ -13,26 +14,80 @@ struct ScreenSwitcher: View {
     @State var pickerVariant = 0
     
     var body: some View {
-        VStack {
-            Picker("", selection: $pickerVariant) {
-                ForEach(0..<pickerOptions.count, id: \.self) {i in
-                    Text(self.pickerOptions[i])
-                        .tag(i)
+        NavStack(transition: .custom(.moveAndFade)){
+            VStack {
+                Picker("", selection: $pickerVariant) {
+                    ForEach(0..<pickerOptions.count, id: \.self) {i in
+                        Text(self.pickerOptions[i])
+                            .tag(i)
+                    }
                 }
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            
-            switch pickerVariant {
-            case 0:
-                NewsScreen(rubric: "Apple")
-            case 1:
-                NewsScreen(rubric: "Tesla")
-            case 2:
-                NewsScreen(rubric: "NASA")
-            default:
-                EmptyView()
+                .pickerStyle(SegmentedPickerStyle())
+                
+                switch pickerVariant {
+                case 0:
+                    NewsScreen(rubric: "Apple")
+                case 1:
+                    NewsScreen(rubric: "Tesla")
+                case 2:
+                    NewsScreen(rubric: "NASA")
+                default:
+                    EmptyView()
+                }
+                NavStackPush(destination: SecondScreen()) {
+                    Text("Push to #2")
+                        .padding()
+                        .background(Color.teal)
+                }
+                
             }
         }
+    }
+}
+
+struct SecondScreen: View {
+    
+    var body: some View {
+        VStack {
+            Text("#2")
+                .font(.system(size: 200))
+            Divider()
+            NavStackPop(destination: .previous) {
+                Text("Pop to #1")
+                    .padding()
+                    .background(Color.teal)
+            }
+            NavStackPush(destination: ThirdScreen()) {
+                Text("Push to #3")
+                    .padding()
+                    .background(Color.teal)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.indigo)
+    }
+}
+
+struct ThirdScreen: View {
+    
+    var body: some View {
+        VStack {
+            Text("#3")
+                .font(.system(size: 200))
+            Divider()
+            NavStackPop(destination: .previous) {
+                Text("Pop to #2")
+                    .padding()
+                    .background(Color.teal)
+            }
+            NavStackPop(destination: .root) {
+                Text("Pop to Root")
+                    .padding()
+                    .background(Color.teal)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.pink)
     }
 }
 
